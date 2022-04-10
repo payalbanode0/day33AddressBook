d856342b2b7c581d5cf66f6113b6abe0a7ed136e
payalbanode0 <payalbanode0@gmail.com>
2022-04-10 23:39:54 +0530

    retrieve data

--- Addrss.sql
+++ Addrss.sql
@@ -15,29 +15,36 @@ email varchar(50)
 
 Insert into Address_Book_Table --UC-3 Insert Data to Table
 (FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
-values('payal','Banode','16nagpur','nagpur','Maharashtra',440034,'8139753564','payalbanode0@gmail.com')
+values('Payal','Banode','Nagpur','Nagpur','Maharashtra',440034,'8139753564','payalbanode0@gmail.com')
 Go
 
 Insert into Address_Book_Table 
 (FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
-values('pooja','Monase','nagpur','nagpur','maharashtra',440034,'918008144657','poojamonase@gmail.com')
+values('Pooja','Monase','Nagpur','Nagpur','maharashtra',440034,'918008144657','poojamonase@gmail.com')
 Go
 
 Insert into Address_Book_Table 
 (FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
-values('erina','disco','torento','torento','Ontario',205532,'918978977310','erina@gmail.com')
+values('Erina','Disco','torento','torento','Ontario',205532,'918978977310','erina@gmail.com')
 Go
+
 
 Update Address_Book_Table -- UC-4 Editing Data using FirstName
-Set City='Warangal'
-where FirstName='Manoj'
+Set City='Nagpur'
+where FirstName='Payal'
 Go
 
 Delete -- UC-5-Delete Record Using FirstName
 from Address_Book_Table
-Where FirstName='Manu'
+Where FirstName='Pooja'
 Go
 
-Select * 
+select * -- UC-6 Retrieve Record Using City
 from Address_Book_Table
+Where City='Nagpur'
+Go
+
+select * --Retrieve Using State
+from Address_Book_Table
+Where _State='Maharashtra'
 Go
