bbe318d7276a650e63e6057a7ddc7360d196661a
payalbanode0 <payalbanode0@gmail.com>
2022-04-10 23:16:33 +0530

    insert

--- Addrss.sql
+++ Addrss.sql
@@ -1,17 +1,32 @@
-Create Database Address_Book
+Create Database Address_Book -- UC-1 Create DataBase
 Go
 
-Create Table Address_Book_Table
+Create Table Address_Book_Table -- UC-2 Create Address Book Table
 (
 FirstName varchar(50),
 LastName varchar(50),
-address varchar,
-City varchar,
-State varchar,
+_address varchar(50),
+City varchar(50),
+_State varchar(50),
 Zip int,
-PhoneNumber varchar(12),--With Country Code,
-email varchar
+PhoneNumber varchar(12),--With Country Code
+email varchar(50)
 )
+
+Insert into Address_Book_Table --UC-3 Insert Data to Table
+(FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
+values('Manoj','Thiparapu','25-4-710 warangal','Kazipet','Telangana',506003,'918106529025','manojthiparapu@gmail.com')
+Go
+
+Insert into Address_Book_Table --UC-3 Insert Data to Table
+(FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
+values('Laxmi','Thiparapu','25-4-710 warangal','Kazipet','Telangana',506003,'918008144657','laxmithiparapu@gmail.com')
+Go
+
+Insert into Address_Book_Table --UC-3 Insert Data to Table
+(FirstName,LastName,_address,City,_State,Zip,PhoneNumber,email) 
+values('Manu','Thiparapu','torento','torento','Ontario',205532,'918978977310','manuthiparapu@gmail.com')
+Go
 
 Select * 
 from Address_Book_Table
