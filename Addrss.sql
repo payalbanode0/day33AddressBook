0abf706316014770d83b5a0516c83ec52b775da1
payalbanode0 <payalbanode0@gmail.com>
2022-04-10 23:13:13 +0530

    create table

--- Addrss.sql
+++ Addrss.sql
@@ -1,3 +1,18 @@
-
 Create Database Address_Book
+Go
+
+Create Table Address_Book_Table
+(
+FirstName varchar(50),
+LastName varchar(50),
+address varchar,
+City varchar,
+State varchar,
+Zip int,
+PhoneNumber varchar(12),--With Country Code,
+email varchar
+)
+
+Select * 
+from Address_Book_Table
 Go
